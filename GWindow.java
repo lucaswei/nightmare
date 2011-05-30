b2f554f1508adea666647c66b3fa39f7a528c603
unknown <lanxan@.(none)>
2011-05-31 02:39:47 +0800

    output

---
+++ GWindow.java
@@ -0,0 +1,61 @@
+import java.awt.Container;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+
+
+public class GWindow {
+	/**
+	 * @param args
+	 */
+	private JFrame game;
+	public Container cp;
+	
+	public GWindow(){
+		
+		game = new JFrame();
+		game.setTitle("Nightmare");
+		game.setLayout(null);
+		game.setSize(800, 600);
+		game.setDefaultCloseOperation(game.EXIT_ON_CLOSE);//Ãö³¬
+		game.setLocationRelativeTo(null);
+		game.setResizable(false);
+		game.setVisible(true);
+		
+		cp = game.getContentPane();
+	}
+
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return game.getWidth();
+	}
+
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return game.getHeight();
+	}
+	
+	public void addKeyListener(Menu menu) {
+		// TODO Auto-generated method stub
+		game.addKeyListener(menu);
+	}
+
+	public void addKeyListener(KeyAdapter keyAdapter) {
+		// TODO Auto-generated method stub
+		game.addKeyListener(keyAdapter);
+	}
+	
+	public void removeKeyListener(Menu menu) {
+		// TODO Auto-generated method stub
+		game.removeKeyListener(menu);
+	}
+
+	public void removeKeyListener(KeyAdapter keyAdapter) {
+		// TODO Auto-generated method stub
+		game.removeKeyListener(keyAdapter);
+	}
+	
+}
+
